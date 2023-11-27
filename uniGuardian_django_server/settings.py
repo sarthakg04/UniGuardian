ca45a4a331aad970664341bdb660a4cedb7eabd4
Deepank Srivastava <deepank@Deepanks-MacBook-Air.local>
2023-11-26 19:14:01 -0800

    Fix build and migrations

--- uniGuardian_django_server/settings.py
+++ uniGuardian_django_server/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'uniGuardian_django_server.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'uniGuardian_django_server.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database

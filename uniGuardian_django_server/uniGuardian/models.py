5cc5ce3c983a050941c167ca02e94d5e8218f1d6
Sarthak Gupta <sarthakanamika@gmail.com>
2023-11-29 14:26:12 -0800

    Deleting Unwanted Files

--- uniGuardian_django_server/uniGuardian/models.py
+++ uniGuardian_django_server/uniGuardian/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
+
 class UserProfile(models.Model):
-   email = models.CharField(primary_key=True, max_length=155)
-   createdAt = models.TimeField(auto_now_add=True)
-   raw_resume = models.TextField(default='')
-   raw_sop = models.TextField(default='')
-   raw_lor1 = models.TextField(default='')
-   raw_lor2 = models.TextField(default='')
-   psychometrics = models.TextField(default='')
-   analysis = models.TextField(default='')
-
-
+    email = models.CharField(primary_key=True, max_length=155)
+    createdAt = models.TimeField(auto_now_add=True)
+    raw_resume = models.TextField(default='')
+    raw_sop = models.TextField(default='')
+    raw_lor1 = models.TextField(default='')
+    raw_lor2 = models.TextField(default='')
+    psychometrics = models.TextField(default='')
+    analysis = models.TextField(default='')
